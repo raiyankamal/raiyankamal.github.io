958f27c698d3fba70e98638a6d427740c518da8b
Raiyan Kamal <raiyan.kamal@gmail.com>
2025-08-31 16:12:42 -0400

    Copy through for assets

--- .eleventy.js
+++ .eleventy.js
@@ -19,6 +19,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
+  eleventyConfig.addPassthroughCopy("src/assets");
+
   return {
     dir: {
       input: "src",
